4fa91876d7ca9f9b05ff8d7c3dd27a791e379a8d
Amar Rajput <amarrajput63@gmail.com>
2021-08-07 11:19:13 +0100

    added socketio client and server as standalone scripts, can be integrated into web app if needed

---
+++ scripts/socketio_client.py
@@ -0,0 +1,61 @@
+import socketio
+import datetime
+import time
+
+from subprocess import check_output
+
+server_address = "localhost" # insert server IP address here
+#server_port = 5000
+server_port = 80
+
+sio = socketio.Client()
+
+@sio.event
+def connect():
+    print("[ INFO ] Incoming connection from something...?")
+
+@sio.event
+def connect_error():
+    print("[ INFO ] Failed to connect.")
+
+@sio.event
+def disconnect():
+    print("[ INFO ] Disconnected from the server.")
+
+
+print('[ INFO ] Connecting to server http://{}:{}...'.format(
+    server_address, server_port))
+
+
+def get_ip_addr():
+    return check_output(['hostname', '-I']).decode().rstrip()
+
+server_connected = False
+
+def ping_server():
+    global server_connected
+    print('Sending ping request to server...')
+    sio.emit(
+        'PING_FROM_CLIENT',
+        {
+            'picam_ip': get_ip_addr()
+        },
+        namespace='/client')
+    server_connected = False
+
+@sio.on('PING_FROM_SERVER', namespace='/client')
+def ping_response(message):
+    global server_connected
+    print('Ping response received: ' + message['server_ip'])
+    server_connected = True
+
+
+sio.connect(
+    'http://{}:{}'.format(server_address, server_port),
+    transports=['websocket'],
+    namespaces=['/client']
+)
+
+while True:
+    ping_server()
+    time.sleep(5)
---
+++ scripts/socketio_server.py
@@ -0,0 +1,73 @@
+from flask import Flask, render_template, request
+from flask_socketio import SocketIO
+
+#import pandas as pd
+#import numpy as np
+#import random
+#import datetime
+
+from subprocess import check_output
+def get_ip_addr():
+    return check_output(['hostname', '-I']).decode().rstrip()
+
+
+app = Flask(__name__)
+socketio = SocketIO(app=app, cors_allowed_origins='*') # in practice, dangerous to allow all origins. Should change * to only domains of interest
+
+@app.route("/", methods=['GET'])
+def index():
+    print("testingtest")
+    return render_template("index.html")
+
+
+@app.route("/login")
+def login_page():
+    return render_template("login.html")
+
+@app.route("/sockets")
+def socket_page():
+    return render_template("sockets.html")
+
+
+@socketio.on('connect', namespace='/client')
+def connect_sock():
+    print("[ INFO ] SocketIO Client Connected: {}".format(request.sid))
+
+@socketio.on('connection_message', namespace='/client')
+def connect_sock(msg):
+    print(msg['message'])
+
+@socketio.on('disconnect', namespace='/client')
+def disconnect_sock():
+    print("[ INFO ] SocketIO Client Disconnected: {}".format(request.sid))
+
+@socketio.on('connect', namespace='/web')
+def connect_web():
+    print("[ INFO ] Web Client Connected: {}".format(request.sid))
+
+@socketio.on('disconnect', namespace='/web')
+def disconnect_web():
+    print("[ INFO ] Web Client Disconnected: {}".format(request.sid))
+
+
+@socketio.on('connect', namespace='/picam_stream')
+def connect_cam():
+    print('Picam Connected!')
+
+
+@socketio.on('PING_FROM_CLIENT', namespace='/client')
+def ping_picam(message):
+    print('PING request received: ' + message['picam_ip'])
+    socketio.emit(
+        'PING_FROM_SERVER',
+        {
+            'server_ip': get_ip_addr()
+        },
+        namespace='/client'
+    )
+    print('PING response sent back to client...')
+
+
+if __name__ == "__main__":
+    print("Test Message...")
+    socketio.run(app=app, host="0.0.0.0", port="5000", debug=True)
